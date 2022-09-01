23b72e6af2ba7d16af6e94b0dc830df0731a374c
Md Shams Tabrej Ansari <rajansari66789@gmail.com>
2022-09-01 20:50:45 +0530

    Time: 19 ms (23.96%), Space: 45.4 MB (24.69%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        char[] array = String.valueOf(x).toCharArray();
+        int end = array.length -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != array[end]){
+                return false;
+            }
+            end--;
+        }
+        return true;
+    }
+}
