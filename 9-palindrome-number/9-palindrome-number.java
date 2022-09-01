fb35b709a90da0b5797545187ef9ae8bb75575d9
Md Shams Tabrej Ansari <rajansari66789@gmail.com>
2022-09-01 20:52:45 +0530

    Time: 18 ms (28.14%), Space: 45.3 MB (27.08%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -3,6 +3,9 @@ class Solution {
         char[] array = String.valueOf(x).toCharArray();
         int end = array.length -1;
         for (int i = 0; i < array.length; i++) {
+            if (i >= end) {
+                return true;
+            }
             if (array[i] != array[end]){
                 return false;
             }
